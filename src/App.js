0076a23e54600f003f418cc1155afbdef17306f9
VivekMalhan666 <amalhan43@gmail.com>
2021-02-07 02:59:08 +0530

    Profile page implemented

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           })
         );
       } else {
-        dispatch(logout);
+        dispatch(logout());
       }
     });
     return unsubscribe;
--- src/Screens/ProfileScreen.js
+++ src/Screens/ProfileScreen.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "../assets/ProfileScreen.css";
 import Nav from "../Components/Nav";
+import { selectUser } from "../features/counter/userSlice";
+import { auth } from "../firebase";
 
 function ProfileScreen() {
+  const user = useSelector(selectUser);
   return (
     <div className="profileScreen">
       <Nav />
       <div className="profileScreen__body">
         <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
