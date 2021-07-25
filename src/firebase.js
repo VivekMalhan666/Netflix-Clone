4dbc578f765c43a4c5dd6ae41a2d055b61b77323
fsociety <fsociety@root>
2021-07-25 20:33:42 +0530

    Improvements - domain issue resolved

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase";
+import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDx19tQNypx0RIlGW2KqxV-8MgAHbZExBQ",
-  authDomain: "netflix-clone-afcdd.firebaseapp.com",
-  databaseURL: "https://netflix-clone-afcdd.firebaseio.com",
-  projectId: "netflix-clone-afcdd",
-  storageBucket: "netflix-clone-afcdd.appspot.com",
-  messagingSenderId: "572015008217",
-  appId: "1:572015008217:web:20ef8c919cc1c84b0561c8",
-  measurementId: "G-W3LF8EXS9L",
+  apiKey: 'AIzaSyD8qn7gMNS89jBZ0DAgJiAjkVqxl5Vc8IE',
+  authDomain: 'netflix-clone-7ee87.firebaseapp.com',
+  databaseURL: 'https://netflix-clone-7ee87-default-rtdb.firebaseio.com',
+  projectId: 'netflix-clone-7ee87',
+  storageBucket: 'netflix-clone-7ee87.appspot.com',
+  messagingSenderId: '551752189971',
+  appId: '1:551752189971:web:d1108577fad2b2f1cd57e6',
+  measurementId: 'G-LBRRQBH1GT',
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
